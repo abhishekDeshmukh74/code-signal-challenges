5de8f02b9fa4856de5f73f5795b4ef4029e6eff6
Abhishek Deshmukh <Abhishekdeshmukh74@gmail.com>
2020-07-26 19:45:38 +0530

    solved minesweeper

---
+++ arcade/intro/level5/minesweeper/index.js
@@ -0,0 +1,68 @@
+function minesweeper(matrix) {
+    const output = []
+    for (let i = 0; i < matrix.length; i++) {
+        output[i] = []
+        for (let j = 0; j < matrix[i].length; j++) {
+            const isAllowed = [i]
+            const jsAllowed = [j]
+            if (i + 1 <= matrix.length - 1) {
+                isAllowed.push(i + 1)
+            }
+            if (i - 1 >= 0) {
+                isAllowed.push(i - 1)
+            }
+            if (j + 1 <= matrix[i].length - 1) {
+                jsAllowed.push(j + 1)
+            }
+            if (j - 1 >= 0) {
+                jsAllowed.push(j - 1)
+            }
+            output[i][j] = getNeighborsAddition(i, j, isAllowed.sort(), jsAllowed.sort())
+        }
+    }
+
+    function getNeighborsAddition(currentI, currentJ, isAllowed, jsAllowed) {
+        let addition = 0
+        for (let k = 0; k < isAllowed.length; k++) {
+            for (let l = 0; l < jsAllowed.length; l++) {
+                if (!(isAllowed[k] === currentI && jsAllowed[l] === currentJ)) {
+                    addition += matrix[isAllowed[k]][jsAllowed[l]] ? 1 : 0
+                }
+            }
+        }
+        return addition
+    }
+    return output
+}
+
+// Best Solution
+/**
+ * The simplest way to accomplish this task is to manually check each direction for every element in the matrix.
+ * We can create a `directions` array to shorten the code for doing so, having it represent the x and y deltas.
+ *
+ * Aside from that, it's rather self-explanatory.
+ */
+
+const directions = [
+    [1, -1], [1, 0], [1, 1],
+    [0, -1], [0, 1],
+    [-1, -1], [-1, 0], [-1, 1]
+];
+
+minesweeper = matrix => matrix.map((row, y) => row.map((col, x) => directions.reduce((count, i) => count += !!(matrix[y + i[0]] && matrix[y + i[0]][x + i[1]]), 0)));
+
+
+console.log(minesweeper([
+    [true, false, false],
+    [false, true, false],
+    [false, false, false]
+]));
+console.log(minesweeper([
+    [false, false, false],
+    [false, false, false]
+]));
+console.log(minesweeper([
+    [true, false, false, true],
+    [false, false, true, false],
+    [true, true, false, true]
+]));
