c6328c6975aca1fac3312244c35436c9be289170
Abhishek Deshmukh <Abhishekdeshmukh74@gmail.com>
2020-08-10 01:10:00 +0530

    solved concatenateArrays

---
+++ arcade/The core/list-forest-edge/concatenate-arrays/index.js
@@ -0,0 +1,10 @@
+function concatenateArrays(a, b) {
+    return a.concat(b)
+}
+
+// Best Solution
+function concatenateArrays(a, b) {
+    return a.concat(b)
+}
+
+console.log(concatenateArrays([2, 2, 1], [10, 11]))
